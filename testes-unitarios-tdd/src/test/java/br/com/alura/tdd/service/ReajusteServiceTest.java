1587239ebae0f93311e31a77b058aad967a83710
Alessandra Souza <le16bits@gmail.com>
2022-02-21 22:39:21 -0300

    add nova classe com cenarios para aplicar tdd

---
+++ testes-unitarios-tdd/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,13 @@
+package br.com.alura.tdd.service;
+
+public class ReajusteServiceTest {
+	
+	
+	/*Cenarios para testar:
+	 * Se o desempenho foi "A desejar", reajuste sera de apenas 3% do salario;
+	 * Se o desempenho foi "bom", reajuste sera de apenas 15% do salario;
+	 * Se o desempenho foi "Otimo", reajuste sera de apenas 20% do salario;
+	 */
+
+	
+}
