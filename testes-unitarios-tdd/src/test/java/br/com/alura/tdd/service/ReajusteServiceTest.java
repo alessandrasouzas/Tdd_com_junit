f197d12036b589823034d19d83638236f5f8bc5c
Alessandra Souza <le16bits@gmail.com>
2022-02-21 22:57:15 -0300

    implementa primeiro cenario

--- testes-unitarios-tdd/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ testes-unitarios-tdd/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -1,5 +1,15 @@
 package br.com.alura.tdd.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
 public class ReajusteServiceTest {
 	
 	
@@ -9,5 +19,14 @@ public class ReajusteServiceTest {
 	 * Se o desempenho foi "Otimo", reajuste sera de apenas 20% do salario;
 	 */
 
-	
+
+	@Test
+	public void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar() {
+		ReajusteService service = new ReajusteService();
+		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000.00"));
+		
+		service.concederReajuste(funcionario,	Desempenho.A_DESEJAR);
+		
+		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());		
+	}
 }
